189b73b589d555f398523546315ebd948f88a8af
KL <1031199085@qq.com>
2016-11-12 19:47:11 +0800

    测试

---
+++ app/src/main/java/com/example/smalu/policebank/Welcome.java
@@ -0,0 +1,23 @@
+package com.example.smalu.policebank;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by KL on 2016/11/12 0012.
+ */
+
+public class Welcome extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome);
+
+        Intent intent = new Intent(Welcome.this,SigninActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
