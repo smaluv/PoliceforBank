9d62599731ee9555ed0515865a7b79026d95d006
dengdailuv <1271879186@qq.com>
2016-11-13 14:08:27 +0800

    备案界面

---
+++ app/src/main/java/com/example/smalu/policebank/activity/BeianActivity.java
@@ -0,0 +1,15 @@
+package com.example.smalu.policebank.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Smalu on 2016/11/13.
+ */
+
+public class BeianActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
